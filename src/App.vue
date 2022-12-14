5c116b197eab02c478908e668e72bd31e8c32728
Niki Bizjak <drobilc@gmail.com>
2022-12-14 12:01:55 +0100

    Add icons and text to app bar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
-    <v-app theme="light">
-    <v-app-bar>
+  <v-app theme="light">
+
+    <v-app-bar elevation="0" color="deep-purple accent-4" dark>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Price comparison</v-toolbar-title>
+
       <v-spacer></v-spacer>
 
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-divider class="mx-4" vertical></v-divider>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-badge :content="0" color="red" offset-x="8" offset-y="8" class="mr-5">
+        <v-btn icon>
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+      </v-badge>
 
     </v-app-bar>
 
