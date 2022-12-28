98deed6243ad22f226e9b975cac27784c58cea21
Niki Bizjak <drobilc@gmail.com>
2022-12-28 17:57:35 +0100

    Update shopping cart design

--- src/views/ShoppingCartView.vue
+++ src/views/ShoppingCartView.vue
@@ -33,22 +33,81 @@ export default {
         </div>
 
         <v-row class="mt-5">
-            <v-col cols="8" offset="2">
+            <v-col cols="10" offset="1">
 
-                <v-list lines="two">
-                    <v-list-item v-for="item in items" :key="item.id" :title="item.name" :subtitle="item.description"
-                        :prepend-avatar="item.image">
+                <div class="shop">
 
-                        <template v-slot:append>
-                            <div class="ml-5">
-                                PRICE
-                            </div>
-                        </template>
-                    </v-list-item>
-                </v-list>
+                    <div class="shop-information d-flex align-center">
+                        <div class="image mr-5">
+                            <v-img width="100" src="https://www.mimovrste.com/_nuxt/img/LogoSl10SI.ffbd456.732.svg" />
+                        </div>
+                        <div class="information ml-5">
+                            <h4>Mimovrste.com</h4>
+                            <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum impedit debitis vel corporis autem, molestiae voluptates iusto reprehenderit earum laboriosam quia eaque veniam, ratione eius repellat in exercitationem aperiam sunt.</p>
+                        </div>
+                    </div>
+
+                    <v-table>
+                        <tbody>
+                            <tr v-for="item in items" :key="item.id">
+                                <td>
+                                    <div class="product-image">
+                                        <v-img aspect-ratio="1" width="64" :src="item.image" contain />
+                                    </div>
+                                </td>
+                                <td>{{ item.name }}</td>
+                                <td>
+                                    <v-text-field
+                                        v-model="numberValue"
+                                        type="number"
+                                        density="compact"
+                                        variant="outlined"
+                                        hide-details
+                                        single-line
+                                        class="quantity"
+                                        value="12"
+                                    />
+                                </td>
+                                <td class="text-center price">12€</td>
+                                <td>
+                                    <v-btn variant="flat" icon>
+                                        <v-icon>mdi-close</v-icon>
+                                    </v-btn>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </v-table>
+
+                </div>
 
             </v-col>
         </v-row>
 
     </div>
 </template>
+
+<style scoped>
+.shop {
+    padding: 2rem;
+    margin-bottom: 2rem;
+}
+.shop-information {
+    background-color: white;
+    padding: 2rem;
+    border-bottom: 1px solid #e0e0e0;
+}
+.shop-information .description {
+    color: #797979;
+    font-size: 0.9em;
+}
+.product-image {
+    padding: 0.5rem;
+}
+.quantity {
+    width: 100px;
+}
+.price {
+    font-weight: bold;
+    font-size: 1.2em;
+}
+</style>
