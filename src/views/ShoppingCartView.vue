5cf24dec817ffb86681e936582a3999ed6f3889f
Niki Bizjak <drobilc@gmail.com>
2022-12-29 15:06:36 +0100

    Update shopping cart design

--- src/views/ShoppingCartView.vue
+++ src/views/ShoppingCartView.vue
@@ -67,55 +67,44 @@ export default {
         <v-row class="mt-5">
             <v-col cols="10" offset="1">
 
-                <div class="shop">
-
-                    <div class="shop-information d-flex align-center">
-                        <div class="image mr-5">
-                            <v-img width="100" src="https://www.mimovrste.com/_nuxt/img/LogoSl10SI.ffbd456.732.svg" />
-                        </div>
-                        <div class="information ml-5">
-                            <h4>Mimovrste.com</h4>
-                            <p class="description">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum impedit debitis vel corporis autem, molestiae voluptates iusto reprehenderit earum laboriosam quia eaque veniam, ratione eius repellat in exercitationem aperiam sunt.</p>
-                        </div>
-                    </div>
-
-                    <v-table>
-                        <tbody>
-                            <tr v-for="item in items" :key="item.id">
-                                <td>
-                                    <div class="product-image">
-                                        <v-img aspect-ratio="1" width="64" :src="item.image" contain />
-                                    </div>
-                                </td>
-                                <td>{{ item.name }}</td>
-                                <td>
-                                    <v-text-field
-                                        type="number"
-                                        density="compact"
-                                        variant="outlined"
-                                        hide-details
-                                        single-line
-                                        class="quantity"
-                                        v-bind:value="item.quantity"
-                                        v-on:input="changeQuantity(item.productId, $event.target.value)"
-                                    />
-                                </td>
-                                <td class="text-center price">12€</td>
-                                <td>
-                                    <v-btn variant="flat" @click="removeFromCart(item.productId)" icon>
-                                        <v-icon>mdi-close</v-icon>
-                                    </v-btn>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </v-table>
-
-                </div>
+                <v-table>
+                    <thead>
+                        <tr>
+                            <th>Slika</th>
+                            <th>Produkt</th>
+                            <th>Količina</th>
+                            <th><!-- Remove from cart button --></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item in items" :key="item.id">
+                            <td>
+                                <div class="product-image">
+                                    <v-img aspect-ratio="1" width="64" :src="item.image" contain />
+                                </div>
+                            </td>
+                            <td>{{ item.name }}</td>
+                            <td>
+                                <v-text-field type="number" density="compact" variant="outlined" hide-details
+                                    single-line class="quantity" v-bind:value="item.quantity"
+                                    v-on:input="changeQuantity(item.productId, $event.target.value)" />
+                            </td>
+                            <td>
+                                <v-btn variant="flat" @click="removeFromCart(item.productId)" icon>
+                                    <v-icon>mdi-close</v-icon>
+                                </v-btn>
+                            </td>
+                        </tr>
+                        <tr v-if="items == null || items.length <= 0">
+                            <td colspan="4">Nakupovalna košarica je prazna.</td>
+                        </tr>
+                    </tbody>
+                </v-table>
 
             </v-col>
         </v-row>
-
     </div>
+
 </template>
 
 <style scoped>
@@ -123,23 +112,23 @@ export default {
     padding: 2rem;
     margin-bottom: 2rem;
 }
+
 .shop-information {
     background-color: white;
     padding: 2rem;
     border-bottom: 1px solid #e0e0e0;
 }
+
 .shop-information .description {
     color: #797979;
     font-size: 0.9em;
 }
+
 .product-image {
     padding: 0.5rem;
 }
+
 .quantity {
     width: 100px;
-}
-.price {
-    font-weight: bold;
-    font-size: 1.2em;
 }
 </style>
